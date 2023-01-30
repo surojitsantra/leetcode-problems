72115f926e352bf15d3d8dd7b78275191e1df63b
surojitsantra <103311508+surojitsantra@users.noreply.github.com>
2023-01-30 11:22:43 +0530

    Time: 0 ms (100.00%), Space: 39 MB (80.79%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 1;
+        
+        int  prev3 = 0, prev2 = 1, prev1 = 1;
+        
+        for(int i = 3; i <=n; i++) {
+            int tn = prev3 +prev2 +prev1;
+            prev3 = prev2;
+            prev2 = prev1;
+            prev1 = tn;
+        }
+        
+        return prev1;
+    }
+}
