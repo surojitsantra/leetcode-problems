e5fe0b3cc7ff7bcccab89be9b3ba34077944a1d1
surojitsantra <103311508+surojitsantra@users.noreply.github.com>
2023-02-26 15:52:03 +0530

    Time: 15 ms (60.65%), Space: 9 MB (42.85%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,9 +3,27 @@ public:
     int minDistance(string word1, string word2) {
         const int M = word1.size(), N = word2.size();
         
-        vector<vector<int>> dp(M +1, vector<int>(N +1, -1));
+        vector<vector<int>> dp(M +1, vector<int>(N +1, 0));
+        for(int i = 0; i <= M; i++) dp[i][0] = i;
+        for(int i = 0; i <= N; i++) dp[0][i] = i;
         
-        return minOpsReq(word1, M, word2, N, dp);
+        for(int len1 = 1; len1 <= M; len1++) {
+            for(int len2 = 1; len2 <= N; len2++) {
+                if(word1[len1 -1] == word2[len2 -1]) {
+                    dp[len1][len2] = dp[len1 -1][len2 -1];
+                } else {
+                    int replaceChar = dp[len1 -1][len2 -1] +1;
+                    int insertChar = dp[len1][len2 -1] +1;
+                    int deleteChar = dp[len1 -1][len2] +1;
+
+                    int minCost = min(replaceChar, insertChar);
+                    minCost = min(minCost, deleteChar);
+                    dp[len1][len2] = minCost;
+                }
+            }
+        }
+        return dp[M][N];
+        // return minOpsReq(word1, M, word2, N, dp);
     }
     
 private:
