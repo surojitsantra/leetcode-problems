0ad7a2b60357891f25107ebaee635447237f15cf
surojitsantra <103311508+surojitsantra@users.noreply.github.com>
2023-04-10 10:32:58 +0530

    Time: 4 ms (39.51%), Space: 6.4 MB (56.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> st;
+        
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            } else {
+                if(st.empty()) return false;
+                char top = st.top();
+                st.pop();
+                if((top == '(' && s[i] != ')')
+                   || (top == '{' && s[i] != '}')
+                   || (top == '[' && s[i] != ']')) return false;
+            }
+        }
+        return(st.empty());
+    }
+};
