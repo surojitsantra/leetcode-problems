d26c076c221b19a3d6156ee53fc56dc7c29f512e
surojitsantra <103311508+surojitsantra@users.noreply.github.com>
2023-05-28 22:57:18 +0530

    Time: 101 ms (35.02%), Space: 10.3 MB (32.53%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     int minCost(int n, vector<int>& cuts) {
-        
         const int SIZE = cuts.size();
-        
-        cuts.insert(cuts.begin(), 0);
-        cuts.push_back(n);
         
         sort(cuts.begin(), cuts.end());
         
-        vector<vector<int>> dp(SIZE +1, vector<int>(SIZE +1, -1));
-        
-        return minimumCost(cuts, 1, SIZE, dp);
+        vector<vector<int>> dp(SIZE +1, vector<int>(SIZE, -1));
+        return minimumCost(cuts, 0, SIZE -1, 0, n, dp);
     }
     
-    int minimumCost(vector<int>& cuts, int i, int j, vector<vector<int>>& dp) {
+    int minimumCost(vector<int>& cuts, int i, int j, int l, int r, vector<vector<int>>& dp) {
         if(i > j) return 0;
         
         if(dp[i][j] != -1) return dp[i][j];
         
         int ans = INT_MAX;
-        
         for(int ind = i; ind <= j; ind++) {
-            int cost = cuts[j +1] -cuts[i -1]
-                        +minimumCost(cuts, i, ind -1, dp)
-                        +minimumCost(cuts, ind +1, j, dp);
+            int cost = r -l
+                        +minimumCost(cuts, i, ind -1, l, cuts[ind], dp)
+                        +minimumCost(cuts, ind +1, j, cuts[ind], r, dp);
             ans = min(ans, cost);
         }
         return dp[i][j] = ans;
