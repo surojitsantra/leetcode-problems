11040bcaeca00d9ccd26a0578aee183b3c7a7955
surojitsantra <103311508+surojitsantra@users.noreply.github.com>
2023-06-04 19:03:49 +0530

    Time: 1 ms (98.97%), Space: 43.9 MB (33.51%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,35 +1,24 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        final int N = isConnected.length;
-        
-        List<Integer>[] adjMatrix = new ArrayList[N];
-        for(int i = 0; i < N; i++) adjMatrix[i] = new ArrayList<>();
-        
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                if(i == j || isConnected[i][j] == 0) continue;
-                adjMatrix[i].add(j);
-            }
-        }
+        final int N = isConnected.length;       
         
         boolean[] visited = new boolean[N];
-        
         int components = 0;
         for(int i = 0; i < N; i++) {
             if(visited[i]) continue;
-            dfs(i, adjMatrix, visited, N);
+            dfs(i, isConnected, visited, N);
             components++;
         }
         
         return components;
     }
     
-    private void dfs(int node, List<Integer>[] adjMatrix, boolean[] visited, final int N) {
+    private void dfs(int node, int[][] isConnected, boolean[] visited, final int N) {
         visited[node] = true;
         
-        for(int v :adjMatrix[node]) {
-            if(visited[v]) continue;
-            dfs(v, adjMatrix, visited, N);
+        for(int i = 0; i < N; i++) {
+            if(isConnected[node][i] == 0 || i == node || visited[i]) continue;
+            dfs(i, isConnected, visited, N);
         }
     }
 }
