15cb18e616197404b90e196b3ec2627b5b9dcf4c
surojitsantra <103311508+surojitsantra@users.noreply.github.com>
2023-06-21 20:40:49 +0530

    Time: 17 ms (76.92%), Space: 55.4 MB (64.62%) - LeetHub

---
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
@@ -0,0 +1,41 @@
+class Solution {
+    public long minCost(int[] nums, int[] cost) {
+        //Bin search
+        int low = Integer.MAX_VALUE, high = Integer.MIN_VALUE;
+        
+        for(int num :nums) {
+            low = Math.min(low, num);
+            high = Math.max(high, num);
+        }
+        
+        low--;
+        high++;
+        
+        long ans = 0;
+        
+        while(low <= high) {
+            int mid = (high -low) /2 +low;
+            long cost1 = getCost(nums, cost, mid);
+            long cost2 = getCost(nums, cost, mid +1);
+            
+            if(cost1 < cost2) {
+                ans = cost1;
+                high = mid -1;
+            } else {
+                low = mid +1;
+            }
+        }        
+        return ans;
+    }
+    
+    private long getCost(int[] nums, int[] cost, int x) {
+        long totalCost = 0;
+        
+        for(int i = 0; i < nums.length; i++) {
+            totalCost += (long)cost[i] *Math.abs(x -nums[i]);
+        }
+        
+        return totalCost;
+    }
+    
+}
