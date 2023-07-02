361072f451c7ae625478d987bb961b7a1024efa5
surojitsantra <103311508+surojitsantra@users.noreply.github.com>
2023-07-02 21:31:50 +0530

    Time: 17 ms (90.91%), Space: 40.2 MB (69.70%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.java
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.java
@@ -1,31 +1,28 @@
 class Solution {
-    int answer = 0;
+    int maxAns = 0;
     
     public int maximumRequests(int n, int[][] requests) {
         
         int employees[] = new int[n];
         
-        maxReqPossible(requests, 0, employees, 0);
-        
-        return answer;
+        return Math.max(0, maxReqPossible(requests, 0, employees));
     }
     
-    private void maxReqPossible(int[][] requests, int i, int[] employees, int count) {
+    private int maxReqPossible(int[][] requests, int i, int[] employees) {
         if(i == requests.length) {
-            for(int num :employees) if(num != 0) return;;
-            answer = Math.max(answer, count);
-            return;
+            for(int num :employees) if(num != 0) return Integer.MIN_VALUE;;
+            return 0;
         }
         
-        //pick
         employees[requests[i][0]]--;
         employees[requests[i][1]]++;
-        maxReqPossible(requests, i +1, employees, count +1);
+        int pick = 1 +maxReqPossible(requests, i +1, employees);
         employees[requests[i][0]]++;
         employees[requests[i][1]]--;
-           
-        //not pick
-        maxReqPossible(requests, i +1, employees, count);
+            
+        int notPick = maxReqPossible(requests, i +1, employees);
+        
+        return Math.max(pick, notPick);
         
     }
 }
