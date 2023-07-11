bbe7860d381c8b54a8e5b183ae93965d2f5f9e5b
surojitsantra <103311508+surojitsantra@users.noreply.github.com>
2023-07-11 20:40:36 +0530

    Time: 14 ms (21.97%), Space: 43.4 MB (10.09%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        buildGraph(root, null, graph);
+        
+        List<Integer> ans = new ArrayList<>();
+        
+        Queue<Integer> helperQueue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        int level = 0;
+        
+        helperQueue.add(target.val);
+        visited.add(target.val);
+        
+        while(!helperQueue.isEmpty()) {
+            int sz = helperQueue.size();
+            while(sz-- > 0) {
+                int val = helperQueue.poll();
+                
+                if(level == k) ans.add(val);
+                
+                for(int neighbor :graph.getOrDefault(val, new ArrayList<>())) {
+                    if(visited.contains(neighbor)) continue;
+                    visited.add(neighbor);
+                    helperQueue.add(neighbor);
+                }
+            }
+            level++;
+            if(level > k) return ans;
+        }
+        
+        return ans;        
+    }
+    
+    private void buildGraph(TreeNode root, TreeNode parent, Map<Integer, List<Integer>> graph) {
+             
+        if(root != null && parent != null) {
+            graph.putIfAbsent(root.val, new ArrayList<>());
+            graph.putIfAbsent(parent.val, new ArrayList<>());
+            graph.get(root.val).add(parent.val);
+            graph.get(parent.val).add(root.val);
+        }
+        if(root.left != null) {
+            buildGraph(root.left, root, graph);
+        }
+        if(root.right != null) {
+            buildGraph(root.right, root, graph);
+        }
+    }
+}
