dd2585f38d8a773a90e0854241c4a86044a73703
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2023-08-05 19:41:46 +0530

    Time: 1 ms (99.88%), Space: 44.2 MB (8.87%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        List<List<TreeNode>> dp = new ArrayList<>(n + 1);
+        for (int i = 0; i <= n; i++) {
+            dp.add(new ArrayList<>());
+        }
+        dp.get(0).add(null);
+
+        for (int numberOfNodes = 1; numberOfNodes <= n; numberOfNodes++) {
+            for (int i = 1; i <= numberOfNodes; i++) {
+                int j = numberOfNodes - i;
+                for (TreeNode left : dp.get(i - 1)) {
+                    for (TreeNode right : dp.get(j)) {
+                        TreeNode root = new TreeNode(i, left, clone(right, i));
+                        dp.get(numberOfNodes).add(root);
+                    }
+                }
+            }
+        }
+        return dp.get(n);
+    }
+
+    private TreeNode clone(TreeNode node, int offset) {
+        if (node == null) {
+            return null;
+        }
+        TreeNode clonedNode = new TreeNode(node.val + offset);
+        clonedNode.left = clone(node.left, offset);
+        clonedNode.right = clone(node.right, offset);
+        return clonedNode;
+    }
+}
