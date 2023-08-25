7659b6a0c332b5b83d84a655b83420df1887ac98
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2023-08-25 20:00:01 +0530

    Time: 1 ms (91.57%), Space: 40.8 MB (66.43%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -6,33 +6,33 @@ class Solution {
         
         if(LEN1 +LEN2 != LEN3) return false;
         
-        int[][][] dp = new int[LEN1][LEN2][LEN3];
-        for(int[][] d: dp) {
-            for(int[] d1 :d) Arrays.fill(d1, -1);
+        int[][] dp = new int[LEN1][LEN2];
+        for(int[] d: dp) {
+            Arrays.fill(d, -1);
         }
         
         return isPossible(s1.toCharArray(),
                          s2.toCharArray(),
                          s3.toCharArray(),
-                         0, 0, 0,
-                         LEN1, LEN2, LEN3, dp) == 1;
+                         0, 0,
+                         LEN1, LEN2, dp) == 1;
     }
     
-    private int isPossible(char[] s1, char[] s2, char[] s3, int i, int j, int k, final int LEN1, final int LEN2, final int LEN3, int[][][] dp) {
-        if(i == LEN1 && j == LEN2 && k == LEN3) return 1;
-        if(k >= LEN3) return 0;
-        if(i < LEN1 && j < LEN2 && dp[i][j][k] != -1) return dp[i][j][k];
+    private int isPossible(char[] s1, char[] s2, char[] s3, int i, int j, final int LEN1, final int LEN2, int[][] dp) {
+        if(i == LEN1 && j == LEN2) return 1;
+        
+        if(i < LEN1 && j < LEN2 && dp[i][j] != -1) return dp[i][j];
         
         int ans = 0;
-        if(ans == 0 && i < LEN1 && s1[i] == s3[k]) {
-            ans = isPossible(s1, s2, s3, i +1, j, k +1, LEN1, LEN2, LEN3, dp);
+        if(ans == 0 && i < LEN1 && s1[i] == s3[i +j]) {
+            ans = isPossible(s1, s2, s3, i +1, j, LEN1, LEN2, dp);
         }
         
-        if(ans == 0 && j < LEN2 && s2[j] == s3[k]) {
-            ans = isPossible(s1, s2, s3, i, j +1, k +1, LEN1, LEN2, LEN3, dp);
+        if(ans == 0 && j < LEN2 && s2[j] == s3[i +j]) {
+            ans = isPossible(s1, s2, s3, i, j +1, LEN1, LEN2, dp);
         }
         
-        if(i < LEN1 && j < LEN2) dp[i][j][k] = ans;
+        if(i < LEN1 && j < LEN2) dp[i][j] = ans;
      
         return ans;
     }
