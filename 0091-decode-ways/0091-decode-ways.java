6ac210d3fd22c2b48dcc31df76f0cdf6b731d906
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2023-12-25 09:50:09 +0530

    Time: 1 ms (84.19%), Space: 41.5 MB (13.24%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,27 +1,42 @@
 class Solution {
     public int numDecodings(String s) {
+        
         final int N = s.length();
-        int[] dp = new int[N];
-        Arrays.fill(dp, -1);
         
-        return noOfWaysDecode(s.toCharArray(), 0, N, dp);
-    }
-    
-    public int noOfWaysDecode(char[] nums, int pos, final int N, int[] dp) {
+        int next1 = 1;
+        int next2 = 1;
         
-        if(pos >= N) return 1;
-        if(nums[pos] == '0') return 0;
-        if(dp[pos] != -1) return dp[pos];
-        
-        int ways = noOfWaysDecode(nums, pos +1, N, dp);
-        if(pos +1 < N) {
-            int num = Integer.valueOf(nums[pos] +"" +nums[pos +1]);
-            if(num <= 26) {
-                ways += noOfWaysDecode(nums, pos +2, N, dp);
+        for(int i = N -1; i >= 0; i--) {
+            int curr = 0;
+            if(s.charAt(i) != '0') {
+                curr = next1;
+                if((i +1 < N) 
+                   && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i +1) <= '6'))) {
+                    curr += next2;
+                }
             }
+            next2 = next1;
+            next1 = curr;
         }
         
-        return dp[pos] = ways;
+        return next1;
     }
+    
+//     public int noOfWaysDecode(char[] nums, int pos, final int N, int[] dp) {
+        
+//         if(pos >= N) return 1;
+//         if(nums[pos] == '0') return 0;
+//         if(dp[pos] != -1) return dp[pos];
+        
+//         int ways = noOfWaysDecode(nums, pos +1, N, dp);
+//         if(pos +1 < N) {
+//             int num = Integer.valueOf(nums[pos] +"" +nums[pos +1]);
+//             if(num <= 26) {
+//                 ways += noOfWaysDecode(nums, pos +2, N, dp);
+//             }
+//         }
+        
+//         return dp[pos] = ways;
+//     }
     
 }
