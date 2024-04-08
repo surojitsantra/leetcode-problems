7ccc778afadd325a8a13ea7b1b6750d8ac75a86b
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-04-08 10:00:20 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (83.84%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba19-26-41-005-data-darkreader-inline-bgcolordivflipkart-divdiv-classcompanytagscontainer-tagoccurence7-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-529be1-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1700-number-of-students-unable-to-eat-lunch/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba19-26-41-005-data-darkreader-inline-bgcolordivflipkart-divdiv-classcompanytagscontainer-tagoccurence7-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-529be1-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1700-number-of-students-unable-to-eat-lunch.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int circularStudentCount = 0;
+        int squareStudentCount = 0;
+        
+        for(int student :students) {
+            circularStudentCount += (-(student -1));
+            squareStudentCount += student;
+        }
+        
+        for(int sandwich :sandwiches) {
+            if(sandwich == 0 && circularStudentCount == 0) {
+                return squareStudentCount;
+            }
+            if(sandwich == 1 && squareStudentCount == 0) {
+                return circularStudentCount;
+            }
+            
+            squareStudentCount -= sandwich;
+            circularStudentCount -= (-(sandwich -1));
+            
+        }
+        
+        return 0;
+    }
+}
