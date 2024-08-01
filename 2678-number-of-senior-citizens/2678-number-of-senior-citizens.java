98a358c433749d3be1c00f43353678c2addb8464
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-08-01 18:57:27 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (21.29%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count = 0;
+        for(String s :details) {
+            if((s.charAt(11) == '6' && s.charAt(12) > '0') 
+               || (s.charAt(11) > '6')) count++;
+        }
+        
+        return count;
+    }
+}
