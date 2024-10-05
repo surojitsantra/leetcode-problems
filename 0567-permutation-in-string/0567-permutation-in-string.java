b0edb502bff63f38ea8f9444e3fde18e1b5fb0f4
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-10-05 09:49:56 +0530

    Time: 8 ms (60.46%), Space: 42.7 MB (61.37%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,49 +1,54 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        final int M = s1.length();
+        final int N = s2.length();
         
-        if(s1.length() > s2.length()) {
-            return false;
-        }
+        if(M > N) return false;
         
-        int[] charFreq = new int[26];
-        for(char ch :s1.toCharArray()) {
-            charFreq[ch -'a']++;
-        }
+        int[] charFreq1 = new int[26];
+        int[] charFreq2 = new int[26];
         
-        for(int i = 0; i < s1.length(); i++) {
-            int idx = s2.charAt(i) -'a';
-            charFreq[idx]--;
-        }
-        
-        if(isSame(charFreq)) {
-            return true;
-        }
-        
-        int i = 0, j = s1.length();
-        while(j < s2.length()) {
-            int idx1 = s2.charAt(i) -'a';
-            int idx2 = s2.charAt(j) -'a';
+        for(int i = 0; i < M; i++) {
+            int idx1 = s1.charAt(i) -'a';
+            int idx2 = s2.charAt(i) -'a';
             
-            charFreq[idx1]++;
-            charFreq[idx2]--;
-            i++;
-            j++;
+            charFreq1[idx1]++;
+            charFreq2[idx2]++;
+        }
+        
+        int matchChars = 0;
+        
+        for(int i = 0; i < 26; i++) {
+            if(charFreq1[i] == charFreq2[i]) matchChars++;
+        }
+        
+        int i = 0, j = M;
+        while(j < N) {
+            int left = s2.charAt(i) -'a';
+            int right = s2.charAt(j) -'a';
             
-            if(isSame(charFreq)) {
+            if(matchChars == 26) {
                 return true;
             }
+            
+            charFreq2[right]++;
+            if(charFreq1[right] == charFreq2[right]) {
+                matchChars++;
+            } else if(charFreq1[right] == charFreq2[right] -1) {
+                matchChars--;
+            }
+            
+            charFreq2[left]--;
+            if(charFreq1[left] == charFreq2[left]) {
+                matchChars++;
+            } else if(charFreq1[left] == charFreq2[left] +1) {
+                matchChars--;
+            }
+            
+            i++;
+            j++;
         }
         
-        return false;
+        return matchChars == 26;
     }
-    
-    private boolean isSame(int[] charFreq) {
-        for(int freq :charFreq) {
-            if(freq != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
 }
