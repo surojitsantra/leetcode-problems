8c2fb4edef65145c5a1c47e7f67b62d72df0c8e7
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-11-16 10:27:06 +0530

    Time: 1 ms (100.00%), Space: 45.3 MB (63.56%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
@@ -1,32 +1,16 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
         final int N = nums.length;
-        Deque<Integer> helperQueue = new LinkedList<>();
         int[] result = new int[N -k +1];
         
-        int i = 0, j = 0, x = 0;
-        while(j < N) {
-            if(j < k) {
-                while(!helperQueue.isEmpty() && nums[helperQueue.peekLast()] +1 != nums[j]) {
-                    helperQueue.removeLast();
-                }
-                helperQueue.addLast(j);
-                if(j == k -1) {
-                    result[x++] = (helperQueue.size() == k)? nums[helperQueue.peekLast()] : -1;
-                }
-                j++;
-            } else {
-                if(!helperQueue.isEmpty() && helperQueue.peekFirst() == i) {
-                    helperQueue.removeFirst();
-                }
-                i++;
-                
-                while(!helperQueue.isEmpty() && nums[helperQueue.peekLast()] +1 != nums[j]) {
-                    helperQueue.removeLast();
-                }
-                helperQueue.addLast(j);
-                result[x++] = (helperQueue.size() == k)? nums[helperQueue.peekLast()] : -1;
-                j++;
+        int streak = 1;
+        int x = 0;
+        for(int j = 0; j < N; j++) {
+            if(j -1 >= 0 && nums[j -1] +1 == nums[j]) streak++;
+            else streak = 1;
+            
+            if(j >= k -1) {
+                result[x++] = (streak >= k)? nums[j] : -1;
             }
         }
         
