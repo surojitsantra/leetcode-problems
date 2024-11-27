6190adce2b9e8792e7d801144a683d0c64b309fc
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-11-27 13:35:46 +0530

    Time: 0 ms (100.00%), Space: 83.6 MB (74.09%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -1,23 +1,40 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
         final int M = mat.length, N = mat[0].length;
-        int r = 0, c = 0;
         
-        while(r >= 0 && c >= 0 && r < M && c < N) {
-            int up = (r -1 >= 0)? mat[r -1][c] : -1;
-            int down = (r +1 < M)? mat[r +1][c] : -1;
+        int l = 0, h = N -1;
+        while(l <= h) {
+            int m = (l +h) /2;
+            int[] maxNoIdx = getMaxNoIdx(mat, m);
+            int r = maxNoIdx[0], c = maxNoIdx[1];
+            int curr = mat[r][c];
             int left = (c -1 >= 0)? mat[r][c -1] : -1;
             int right = (c +1 < N)? mat[r][c +1] : -1;
-            int curr = mat[r][c];
             
-            if(up > curr) r--;
-            else if(down > curr) r++;
-            else if(left > curr) c--;
-            else if(right > curr) c++;
-            else return new int[]{r, c};
+            if(curr < left) {
+                h = m -1;
+            } else if(curr < right) {
+                l = m +1;
+            } else {
+                return maxNoIdx;
+            }
             
         }
         
         return new int[]{-1, -1};
     }
+    
+    private int[] getMaxNoIdx(int[][] mat, int c) {
+        int[] maxNoIdx = new int[]{0, c};
+        
+        for(int r = 0; r < mat.length; r++) {
+            if(mat[r][c] > mat[maxNoIdx[0]][maxNoIdx[1]]) {
+                maxNoIdx[0] = r;
+                maxNoIdx[1] = c;
+            }
+        }
+        
+        return maxNoIdx;
+    }
+    
 }
