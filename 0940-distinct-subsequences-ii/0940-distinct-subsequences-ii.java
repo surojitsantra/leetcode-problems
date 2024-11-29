a7c93522f718b7b7d8e4dd0bd5dab4ca3ca05198
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-11-29 18:47:43 +0530

    Time: 2 ms (96.86%), Space: 41.9 MB (61.95%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -2,27 +2,21 @@ class Solution {
     public int distinctSubseqII(String s) {
         final int MOD = 1_000_000_007;
         final int N = s.length();
-        int[] lastOccuranceIdx = new int[26];
-        int[] dp = new int[N +1];
-        dp[0] = 1;
+        long[] lastOccurance = new long[26];
         
-        for(int i = 1; i <= N; i++) {
-            int x = (s.charAt(i -1) -'a');
+        long count = 1;
+        
+        for(char ch :s.toCharArray()) {
+            long prevCount = count;
+            count = (count *2) %MOD;
             
-            dp[i] = dp[i -1] *2;
-            dp[i] %= MOD;
+            count -= lastOccurance[ch -'a'];
+            count %= MOD;
             
-            if(lastOccuranceIdx[x] > 0) {
-                dp[i] -= dp[lastOccuranceIdx[x] -1];
-            }
-            dp[i] %= MOD;
-            
-            lastOccuranceIdx[x] = i;
+            lastOccurance[ch -'a'] = prevCount;
         }
-        
-        dp[N]--;
-        if (dp[N] < 0) dp[N] += MOD;
-        
-        return dp[N];
+        count--;
+        if (count < 0) count += MOD;
+        return (int)count;
     }
 }
