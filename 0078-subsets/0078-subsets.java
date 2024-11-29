34359a98622755423743d8a8c36e0a0e74de1213
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-11-29 12:22:21 +0530

    Time: 1 ms (55.29%), Space: 42.4 MB (92.60%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        final int N = nums.length;
+        List<List<Integer>> subsets = new ArrayList<>();
+        
+        for(int i = 0; i < Math.pow(2, N); i++) {
+            List<Integer> set = new ArrayList<>();
+            
+            for(int j = 0; j < N && j < 32; j++) {
+                boolean take = ((i &(1 <<j)) == 0);
+                if(take) {
+                    set.add(nums[j]);
+                }
+            }
+            
+            subsets.add(set);
+        }
+        
+        return subsets;
+    }
+}
