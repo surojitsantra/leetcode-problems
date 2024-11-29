ac9c0946b671a493f76479158b9c8024d89cf0d1
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-11-29 12:23:08 +0530

    Time: 1 ms (55.29%), Space: 42.7 MB (52.57%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i = 0; i < Math.pow(2, N); i++) {
             List<Integer> set = new ArrayList<>();
             
-            for(int j = 0; j < N && j < 32; j++) {
+            for(int j = 0; j < Math.min(N, 32); j++) {
                 boolean take = ((i &(1 <<j)) == 0);
                 if(take) {
                     set.add(nums[j]);
