be6f1d99d0610e1b7be3d87737d48d029a2547e0
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-12-01 12:59:01 +0530

    Time: 6 ms (93.25%), Space: 44.9 MB (12.63%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -4,32 +4,24 @@ class Solution {
         
         int[] charCount = new int[26];
         int start = 0;
-        int maxCharCount = 0;
+        int maxCharFreq = 0;
         int maxLen = 0;
         for(int end = 0; end < N; end++) {
-            charCount[s.charAt(end) -'A']++;
+            char endChar = s.charAt(end);
+            charCount[endChar -'A']++;
             
-            maxCharCount = Math.max(maxCharCount, charCount[s.charAt(end) - 'A']);
-            
-            while(end -start +1 -maxCharCount > k) {
-                charCount[s.charAt(start) -'A']--;
+            maxCharFreq = Math.max(maxCharFreq, charCount[endChar -'A']);
+            int substrLen = end -start +1;
+            while(start <= end && substrLen -maxCharFreq > k) {
+                char startChar = s.charAt(start);
+                charCount[startChar -'A']--;
                 start++;
-                // maxCharCount = getMaxCount(charCount);
+                substrLen--;
             }
             
             maxLen = Math.max(maxLen, end -start +1);
-            
         }
         
         return maxLen;
     }
-    
-    private int getMaxCount(int[] charCount) {
-        int maxCharCount = 0;
-        for(int count :charCount) {
-            maxCharCount = Math.max(maxCharCount, count);
-        }
-        return maxCharCount;
-    }
-    
 }
