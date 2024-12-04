b94991a4384184b0a98564f516c084247a8fb722
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-12-04 10:07:22 +0530

    Time: 0 ms (100.00%), Space: 46 MB (52.09%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        return delete(root, key);
+    }
+    
+    private TreeNode delete(TreeNode node, int key) {
+        if(node == null) {
+            return node;
+        }
+        if(node.val == key) {
+            if(node.left == null) {
+                return node.right;
+            } else if(node.right == null) {
+                return node.left;
+            } else {
+                TreeNode left = node.left;
+                TreeNode right = node.right;
+                TreeNode extreameLeft = getExtreamLeft(right);
+                extreameLeft.left = left;
+                
+                return right;
+            }
+        } else {
+            node.left = delete(node.left, key);
+            node.right = delete(node.right, key);
+        }
+        
+        return node;
+    }
+    
+    private TreeNode getExtreamLeft(TreeNode node) {
+        while(node != null && node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+    
+}
