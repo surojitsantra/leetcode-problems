9fe667e58486b03e6c90d76b06faea49c71ae9d0
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-12-07 23:46:27 +0530

    Time: 29 ms (19.19%), Space: 50.6 MB (71.15%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,26 +3,43 @@ class Solution {
         final int M = text1.length(), N = text2.length();
         int[][] dp = new int[M][N];
         for(int[] d :dp) Arrays.fill(d, -1);
+
+        char[] str1 = text1.toCharArray();
+        char[] str2 = text2.toCharArray();
         
-        return lcs(text1.toCharArray(), M -1, text2.toCharArray(), N -1, dp);
+//         //base case
+//         for(int x = 0; x < N; x++) {
+//             dp[0][x] = (str1[0] == str2[x])? 1 : 0;
+//         }
+//         for(int x = 0; x < M; x++) {
+//             dp[x][0] = (str1[x] == str2[0])? 1 : 0;
+//         }
+        
+//         for(int i = 1; i < M; i++) {
+//             for(int j = 1; j < N; j++) {
+//                 if(str1[i] == str2[j]) {
+//                     dp[i][j] = 1 +dp[i -1][j -1];
+//                     continue;
+//                 }
+
+//                 int firstSkip = dp[i -1][j];
+//                 int secondSkip = dp[i][j -1];
+
+//                 dp[i][j] = Math.max(firstSkip, secondSkip);
+//             }
+//         }
+        
+//         return dp[M -1][N -1];
+        
+        return lcs(str1, M -1, str2, N -1, dp);
         
     }
     
     
     private int lcs(char[] str1, int i, char[] str2, int j, int[][] dp) {
         
-        if(i == 0) {
-            for(int x = 0; x <= j; x++) {
-                if(str1[0] == str2[x]) return 1;
-            }
-            return 0;
-        }
-        if(j == 0) {
-            for(int x = 0; x <= i; x++) {
-                if(str1[x] == str2[0]) return 1;
-            }
-            return 0;
-        }
+        if(i < 0 || j < 0) return 0;
+        
         if(dp[i][j] != -1) return dp[i][j];
         
         if(str1[i] == str2[j]) {
