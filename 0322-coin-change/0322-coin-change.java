ba7c787ffad5d99e12dba22ea825369fc1a02677
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-12-07 22:38:25 +0530

    Time: 16 ms (56.56%), Space: 45.6 MB (10.38%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -6,32 +6,36 @@ class Solution {
         final int N = coins.length;
         
         int[][] dp = new int[N][amount +1];
-        for(int[] d :dp) Arrays.fill(d, -1);
+        // for(int[] d :dp) Arrays.fill(d, MAX_NO);
         
-        int x = minCoins(coins, N -1, amount, dp);
+        //buttom up
+        //base case
+        
+        
+        for(int amt = 0; amt <= amount; amt++) {
+            if(amt %coins[0] == 0) dp[0][amt] = amt /coins[0];
+            else dp[0][amt] = MAX_NO;
+        }
+        for(int pos = 0; pos < N; pos++) {
+            dp[pos][0] = 0;
+        }
+        
+        for(int pos = 1; pos < N; pos++) {
+            for(int amt = 1; amt <= amount; amt++) {
+                
+                int skip = dp[pos -1][amt];
+                
+                int take = MAX_NO;
+                if(amt >= coins[pos]) {
+                    take = dp[pos][amt -coins[pos]] +1;
+                }
+
+                dp[pos][amt] = Math.min(skip, take);
+            }
+        }
+        
+        int x = dp[N -1][amount];
         return (x == MAX_NO)? -1 : x;
-    }
-    
-    private int minCoins(int[] coins, int pos, int amount, int[][] dp) {
-        if(amount == 0) {
-            return 0;
-        }
-        if(pos == 0) {
-            //do stuff
-            if(amount %coins[0] == 0) return amount /coins[0];
-            return MAX_NO;
-        }
-        if(dp[pos][amount] != -1) {
-            return dp[pos][amount];
-        }
-        
-        int skip = minCoins(coins, pos -1, amount, dp);
-        int take = MAX_NO;
-        if(amount >= coins[pos]) {
-            take = minCoins(coins, pos, amount -coins[pos], dp) +1;
-        }
-        
-        return dp[pos][amount] = Math.min(skip, take);
     }
     
 }
