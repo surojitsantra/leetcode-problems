720fd1dd943002342e41d23270bcea4a82bfe47b
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-12-07 23:24:55 +0530

    Time: 34 ms (8.84%), Space: 50.8 MB (48.64%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        final int M = text1.length(), N = text2.length();
+        int[][] dp = new int[M][N];
+        for(int[] d :dp) Arrays.fill(d, -1);
+        
+        return lcs(text1.toCharArray(), M -1, text2.toCharArray(), N -1, dp);
+        
+    }
+    
+    
+    private int lcs(char[] str1, int i, char[] str2, int j, int[][] dp) {
+        
+        if(i == 0) {
+            for(int x = 0; x <= j; x++) {
+                if(str1[0] == str2[x]) return 1;
+            }
+            return 0;
+        }
+        if(j == 0) {
+            for(int x = 0; x <= i; x++) {
+                if(str1[x] == str2[0]) return 1;
+            }
+            return 0;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(str1[i] == str2[j]) {
+            return dp[i][j] = 1 +lcs(str1, i -1, str2, j -1, dp);
+        }
+        
+        int firstSkip = lcs(str1, i -1, str2, j, dp);
+        int secondSkip = lcs(str1, i, str2, j -1, dp);
+        int bothSkip = lcs(str1, i -1, str2, j -1, dp);
+        
+        return dp[i][j] = Math.max(firstSkip, Math.max(secondSkip, bothSkip));
+        
+    }
+    
+}
