fc01d1a7395b43e4214e2f8d7e91d3297277f9ff
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-12-07 22:41:06 +0530

    Time: 15 ms (77.77%), Space: 45.4 MB (13.52%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -5,36 +5,37 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         final int N = coins.length;
         
-        int[][] dp = new int[N][amount +1];
-        // for(int[] d :dp) Arrays.fill(d, MAX_NO);
+        int[] prev = new int[amount +1];
         
         //buttom up
         //base case
         
-        
         for(int amt = 0; amt <= amount; amt++) {
-            if(amt %coins[0] == 0) dp[0][amt] = amt /coins[0];
-            else dp[0][amt] = MAX_NO;
+            if(amt %coins[0] == 0) prev[amt] = amt /coins[0];
+            else prev[amt] = MAX_NO;
         }
-        for(int pos = 0; pos < N; pos++) {
-            dp[pos][0] = 0;
-        }
+        prev[0] = 0;
         
         for(int pos = 1; pos < N; pos++) {
+            int[] curr = new int[amount +1];
+            curr[0] = 0;
+            
             for(int amt = 1; amt <= amount; amt++) {
                 
-                int skip = dp[pos -1][amt];
+                int skip = prev[amt];
                 
                 int take = MAX_NO;
                 if(amt >= coins[pos]) {
-                    take = dp[pos][amt -coins[pos]] +1;
+                    take = curr[amt -coins[pos]] +1;
                 }
 
-                dp[pos][amt] = Math.min(skip, take);
+                curr[amt] = Math.min(skip, take);
             }
+            
+            prev = curr;
         }
         
-        int x = dp[N -1][amount];
+        int x = prev[amount];
         return (x == MAX_NO)? -1 : x;
     }
     
