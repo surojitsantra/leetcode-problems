fadedcdc4a75949cd45d5ea94b20e7eab273e256
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-12-08 13:51:02 +0530

    Time: 76 ms (30.68%), Space: 64 MB (32.64%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,13 +1,34 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        return getProfitBottomUp(prices, 2);
+    }
+    
+    private int getProfitBottomUp(int[] prices, int txn) {
         final int N = prices.length;
-        int[][][] dp = new int[N][2][3];
+        int[][][] dp = new int[N +1][2][txn +1];
         
-        for(int[][] x :dp) {
-            for(int[] y :x) Arrays.fill(y, -1);
-        }
+        for(int day = N -1; day >= 0; day--) {
+            for(int buy = 0; buy <= 1; buy++) {
+                for(int cap = 1; cap <= txn; cap++) {
+                    
+                    if(buy == 1) {
+                        int take = -prices[day] +dp[day +1][0][cap];
+                        int skip = dp[day +1][1][cap];
+
+                        dp[day][buy][cap] = Math.max(take, skip);
+
+                    } else {
+                        int sell = prices[day] +dp[day +1][1][cap -1];
+                        int skip = dp[day +1][0][cap];
+
+                        dp[day][buy][cap] = Math.max(sell, skip);
+                    }
+                
+                }
+            }
+        }        
         
-        return getProfit(prices, 0, 1, 2, N, dp);
+        return dp[0][1][2];
     }
     
     private int getProfit(int[] prices, int day, int buy, int cap, int N, int[][][] dp) {
