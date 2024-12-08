05c99cfa014c023759764f37880b23be3b998024
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-12-08 14:08:32 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (18.48%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,12 +1,45 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        return getProfitBottomUp(prices);
+    }
+    
+    
+    private int getProfitBottomUp(int[] prices) {
         final int N = prices.length;
         
-        int[][] dp = new int[N][2];
-        for(int[] d :dp) Arrays.fill(d, -1);
+        int[] ahed2 = new int[2];
+        int[] ahed1 = new int[2];
         
-        return getProfit(prices, 0, 1, N, dp);
+        for(int day = N -1; day >= 0; day--) {
+            int[] curr = new int[2];
+            
+            for(int buy = 0; buy <= 1; buy++) {
+                
+                if(buy == 1) {
+                    int take = -prices[day] +ahed1[0];
+                    int skip = ahed1[1];
+
+                    curr[buy] = Math.max(take, skip);
+
+                } else {
+                    int sell = prices[day];
+                    if(day +2 < N) {
+                        sell += ahed2[1];
+                    }
+                    int skip = ahed1[0];
+
+                    curr[buy] = Math.max(sell, skip);
+                }
+                
+            }
+            ahed2 = ahed1;
+            ahed1 = curr;
+            
+        }
+        
+        return ahed1[1];
     }
+    
     
     private int getProfit(int[] prices, int day, int buy, int N, int[][] dp) {
         if(day == N) {
