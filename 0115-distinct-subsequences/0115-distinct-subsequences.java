c35fb081cc24aa344e7de6b624976f132378e115
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-12-08 12:01:17 +0530

    Time: 9 ms (94.26%), Space: 51.5 MB (20.73%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -3,14 +3,43 @@ class Solution {
         char[] s1 = s.toCharArray();
         char[] s2 = t.toCharArray();
         
+        return occuranceButtomUp(s1, s2);
+        
+    }
+    
+    
+    private int occuranceButtomUp(char[] s1, char[] s2) {
+        
         final int M = s1.length;
         final int N = s2.length;
+            
+        int[][] dp = new int[M +1][N +1];
         
-        int[][] dp = new int[M][N];
-        for(int[] d :dp) Arrays.fill(d, -1);
+        //base case
+        //start dp idx from 1
+        //if(j < 0) dp[i][0] = 1
         
-        return occurance(s1, M -1, s2, N -1, dp);
+        for(int i = 0; i < M; i++) {
+            dp[i][0] = 1;
+        }
         
+        for(int i = 0; i < M; i++) {
+            for(int j = 0; j < N; j++) {
+                
+                int dpI = i +1;
+                int dpJ = j +1;
+                
+                if(s1[i] == s2[j]) {
+                    int take = dp[dpI -1][dpJ -1];
+                    int skip = dp[dpI -1][dpJ];
+                    dp[dpI][dpJ] = take +skip;
+                } else {
+                    dp[dpI][dpJ] = dp[dpI -1][dpJ];
+                }
+            }
+        }
+        
+        return dp[M][N];
     }
     
     
@@ -23,9 +52,6 @@ class Solution {
             return 0;
         }
         
-        if(i < 0 || j < 0) {
-            return 0;
-        }
         if(dp[i][j] != -1) return dp[i][j];
         
         if(s1[i] == s2[j]) {
