cf3148ee0bd6d47352e4394a8b590a46d7cf3952
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-12-08 13:12:51 +0530

    Time: 2 ms (14.73%), Space: 45.3 MB (93.82%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,16 +1,49 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        return getProfitBottomUp(prices, 1);
+    }
+    
+    private int getProfitBottomUp(int[] prices, int bag) {
         final int N = prices.length;
+        int[][] dp = new int[N +1][2];
         
-        int[][] dp = new int[N][2];
-        for(int[] d :dp) Arrays.fill(d, -1);
+        //base case
+        dp[N][0] = dp[N][1] = 0;
         
-        return profit(prices, 0, 0, 1, dp);
+        for(int day = N -1; day >= 0; day--) {
+            for(int used = 0; used <= bag; used++) {
+                int profit = 0;
+
+                //can buy?
+                for(int i = used; i < bag; i++) {
+                    int buy = dp[day +1][used +1] -prices[day];
+                    int skip = dp[day +1][used];
+
+                    profit = Math.max(profit, buy);
+                    profit = Math.max(profit, skip);
+
+                }
+
+                //can sell?
+                for(int i = 0; i < used; i++) {
+                    int sell = dp[day +1][used -1] +prices[day];
+                    int skip = dp[day +1][used];
+
+                    profit = Math.max(profit, sell);
+                    profit = Math.max(profit, skip);
+
+                }
+
+                dp[day][used] = profit;
+            }
+        }
+        
+        return dp[0][0];
     }
     
     private int profit(int[] prices, int day, int used, int bag, int[][] dp) {
         
-        if(day >= prices.length) {
+        if(day == prices.length) {
             return 0;
         }
         if(dp[day][used] != -1) return dp[day][used];
