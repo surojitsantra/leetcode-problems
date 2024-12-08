5c895a7020abb0985aae08f244d697d85cf98304
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-12-08 15:05:31 +0530

    Time: 36 ms (74.52%), Space: 43.7 MB (97.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,36 +1,60 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         
-        return lisBottomUp(nums);
+        return lisInOtherWay(nums);
+    }
+    
+    private int lisInOtherWay(int[] nums) {
+        final int N = nums.length;
+        
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+        
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] +1);
+                }
+            }
+        }
+        
+        int maxLen = 0;
+        for(int n :dp) maxLen = Math.max(n, maxLen);
+        
+        return maxLen;
     }
     
     private int lisBottomUp(int[] nums) {
         final int N = nums.length;
-        int[][] dp = new int[N][N +1];
+        int[] prev = new int[N +1];
         
         //base case
         for(int last = 0; last <= N; last++) {
-            if(last == N || nums[0] < nums[last]) dp[0][last] = 1;
-            else dp[0][last] = 0;
+            if(last == N || nums[0] < nums[last]) prev[last] = 1;
+            else prev[last] = 0;
         }
         
         for(int pos = 1; pos < N; pos++) {
+            int[] curr = new int[N +1];;
+            
             for(int last = N; last >= 0; last--) {
                 
-                int skip = dp[pos -1][last];
+                int skip = prev[last];
 
                 //take
                 int take = 0;
                 if(last == N || nums[pos] < nums[last]) {
-                    take = dp[pos -1][pos] +1;
+                    take = prev[pos] +1;
                 }
 
-                dp[pos][last] = Math.max(skip, take);
+                curr[last] = Math.max(skip, take);
                 
             }
+            
+            prev = curr;
         }
         
-        return dp[N -1][N]; 
+        return prev[N]; 
     }
     
     
