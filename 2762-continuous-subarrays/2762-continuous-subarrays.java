d8723d605d3a8131be0bb5679868de4b1e559927
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-12-14 19:13:48 +0530

    Time: 22 ms (59.60%), Space: 56.8 MB (92.49%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.java
+++ 2762-continuous-subarrays/2762-continuous-subarrays.java
@@ -2,23 +2,38 @@ class Solution {
     public long continuousSubarrays(int[] nums) {
         final int N = nums.length;
         long count = 0;
-        TreeMap<Integer, Integer> tMap = new TreeMap<>();
         
         int start = 0;
+        Deque<Integer> maxQueue = new LinkedList<>();
+        Deque<Integer> minQueue = new LinkedList<>();
+        
         for(int end = 0; end < N; end++) {
-            tMap.put(nums[end], tMap.getOrDefault(nums[end], 0) +1);
             
-            while(start <= end 
-                 && tMap.lastEntry().getKey() -tMap.firstEntry().getKey() > 2) {
+            while(!maxQueue.isEmpty() 
+                  && nums[end] >= nums[maxQueue.peekLast()]) {
+                maxQueue.removeLast();
+            }
+            while(!minQueue.isEmpty() 
+                  && nums[end] <= nums[minQueue.peekLast()]) {
+                minQueue.removeLast();
+            }
+            
+            maxQueue.addLast(end);
+            minQueue.addLast(end);
+            
+            while(nums[maxQueue.peekFirst()] -nums[minQueue.peekFirst()] > 2) {
                 
-                tMap.put(nums[start], tMap.getOrDefault(nums[start], 0) -1);
-                if(tMap.get(nums[start]) == 0) {
-                    tMap.remove(nums[start]);
+                if(start == maxQueue.peekFirst()) {
+                    maxQueue.removeFirst();
+                }
+                while(start == minQueue.peekFirst()) {
+                    minQueue.removeFirst();
                 }
                 start++;
             }
             
             count += (end -start +1);
+            
         }
         
         return count;
