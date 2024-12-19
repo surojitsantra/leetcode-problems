921530514bf13b2e3c5f3a60887649b91544ced9
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-12-19 17:03:42 +0530

    Time: 3 ms (66.73%), Space: 44.9 MB (72.94%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -13,71 +13,22 @@
  *     }
  * }
  */
-
-class BSTIterator {
-
-    private Stack<TreeNode> st;
-    private TreeNode root;
-    private boolean isReversed;
-        
-    public BSTIterator(TreeNode root, boolean isReversed) {
-        this.root = root;
-        this.st = new Stack<>();
-        this.isReversed = isReversed;
-        
-        pushAll(root);
-    }
-    
-    public int next() {
-        TreeNode node = st.pop();
-        int val = node.val;
-        
-        if(isReversed) {
-            node = node.left;
-        } else {
-            node = node.right;
-        }
-        pushAll(node);
-        
-        return val;
-    }
-    
-    private void pushAll(TreeNode node) {
-        while(node != null) {
-            st.push(node);
-            if(isReversed) {
-                node = node.right;
-            } else {
-                node = node.left;
-            }
-            
-        }
-    }
-    
-    public boolean hasNext() {
-        return !st.isEmpty();
-    }
-}
-
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        BSTIterator itrAsc = new BSTIterator(root, false);
-        BSTIterator itrDesc = new BSTIterator(root, true);
+        return isPresent(root, k, new HashSet<>());
+    }
+    
+    private boolean isPresent(TreeNode node, int k, Set<Integer> nums) {
+        if(node == null) return false;
         
-        int val1 = itrAsc.next();
-        int val2 = itrDesc.next();
-        while(val1 < val2) {
-            
-            if(val1 +val2 == k) {
-                return true;
-            } else if(val1 +val2 < k) {
-                val1 = itrAsc.next();
-            } else {
-                val2 = itrDesc.next();
-            }
-            
+        if(nums.contains(k -node.val)) {
+            return true;
         }
         
-        return false;
+        nums.add(node.val);
+        
+        return isPresent(node.left, k, nums) || isPresent(node.right, k, nums);
+        
     }
+    
 }
