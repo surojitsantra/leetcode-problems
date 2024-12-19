9d39d9f12ace15da4b5cbf7d12e148b1a36e64d6
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-12-19 20:48:23 +0530

    Time: 1 ms (99.82%), Space: 45.6 MB (26.42%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,41 +1,48 @@
 class Solution {
+    
+    private static final int MAX_VALUE = 100_000_000_0;
+    
     public int minFallingPathSum(int[][] matrix) {
-        final int N = matrix.length;
-        int[][] dp = new int[N][N];
+        final int M = matrix.length, N = matrix[0].length;
+        int[][] dp = new int[M][N];
+        
+        for(int[] d :dp) {
+            Arrays.fill(d, Integer.MIN_VALUE);
+        }
+        
+        
+        int minSum = MAX_VALUE;
         
         for(int c = 0; c < N; c++) {
-            dp[0][c] = matrix[0][c];
+            minSum = Math.min(minSum, minPathSum(matrix, M -1, c, M, N, dp));
         }
         
-        for(int r = 1; r < N; r++) {
-            for(int c = 0; c < N; c++) {
-        
-                int leftUp = Integer.MAX_VALUE;
-                if(c -1 >= 0) {
-                    leftUp = dp[r -1][c -1];
-                }
-
-                int up = dp[r -1][c];
-
-                int rightUp = Integer.MAX_VALUE;
-                if(c +1 < N) {
-                    rightUp = dp[r -1][c +1];
-                }
-
-                int mini = Math.min(leftUp, rightUp);
-                mini = Math.min(mini, up);
-
-                dp[r][c] = matrix[r][c] +mini;
-            }
+        return minSum;
+    }
+    
+    private int minPathSum(int[][] matrix, int r, int c, int M, int N, int[][] dp) {
+        if(r == 0) {
+            return matrix[r][c];
+        }
+        if(dp[r][c] != Integer.MIN_VALUE) {
+            return dp[r][c];
         }
         
-        int mini = Integer.MAX_VALUE;
-        for(int c = 0; c < N; c++) {
-            int x = dp[N -1][c];
-            mini = Math.min(mini, x);
+        int up = minPathSum(matrix, r -1, c, M, N, dp);
+        int upLeft = MAX_VALUE;
+        if(c -1 >= 0) {
+            upLeft = minPathSum(matrix, r -1, c -1, M, N, dp);
         }
         
-        return mini;
+        int upRight = MAX_VALUE;
+        if(c +1 < N) {
+            upRight = minPathSum(matrix, r -1, c +1, M, N, dp);
+        }
+        
+        int minSum = Math.min(upLeft, upRight);
+        minSum = Math.min(minSum, up);
+        
+        return dp[r][c] = minSum +matrix[r][c];
     }
     
 }
