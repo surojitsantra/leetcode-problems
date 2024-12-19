4362ab2feb61cd79f33e448370e71adce8e0af4e
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2024-12-19 20:11:54 +0530

    Time: 1 ms (99.65%), Space: 44 MB (75.96%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,36 +1,31 @@
 class Solution {
+    private static final int MAX_VALUE = 1000000000;
+    
     public int minimumTotal(List<List<Integer>> triangle) {
-        int M = triangle.size();
-        int N = triangle.get(M -1).size();
+        final int M = triangle.size();
+        int[][] dp = new int[M][triangle.get(M -1).size()];
         
-        int[] down = new int[N];
+        for(int[] d :dp) Arrays.fill(d, Integer.MIN_VALUE);
         
-        for(int r = M -1; r >= 0; r--) {
-            N = triangle.get(r).size();
-            int[] curr = new int[N];
-            
-            for(int c = N -1; c >= 0; c--) {
-                int one = Integer.MAX_VALUE;
-                if(r +1 < M) {
-                    one = down[c];
-                }
-                
-                int two = Integer.MAX_VALUE;
-                if(r +1 < M && c +1 < N +1) {
-                    two = down[c +1];
-                }
-                if(one == two && one == Integer.MAX_VALUE) {
-                    one = 0;
-                }
-
-                curr[c] = Math.min(one, two) +triangle.get(r).get(c);
-            }
-            
-            down = curr;
-            
+        return minSum(triangle, 0, 0, dp);
+    }
+    
+    private int minSum(List<List<Integer>> triangle, int r, int c, int[][] dp) {
+        if(r == triangle.size() -1) {
+            return triangle.get(r).get(c);
         }
         
-        return down[0];
+        if(dp[r][c] != Integer.MIN_VALUE) return dp[r][c];
+        
+        int goStraight = minSum(triangle, r +1, c, dp);
+        
+        int goRightCorner = MAX_VALUE;
+        if(c +1 < triangle.get(r).size() +1) {
+            goRightCorner = minSum(triangle, r +1, c +1, dp);
+        }
+        
+        return dp[r][c] = Math.min(goRightCorner, goStraight) +triangle.get(r).get(c);
+        
     }
     
 }
