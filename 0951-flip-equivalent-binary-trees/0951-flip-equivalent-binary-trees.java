f8d0d95ff6e02a679ba3d8c64b7add681d11f921
Surojit Santra <103311508+surojitsantra@users.noreply.github.com>
2026-04-18 10:26:47 +0530

    Time: 0 ms (100%), Space: 43 MB (90.04%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
@@ -15,25 +15,30 @@
  */
 class Solution {
     public boolean flipEquiv(TreeNode root1, TreeNode root2) {
-        if(root1 == null && root2 == null) {
-            return true;
-        }
-        if(root1 == null) {
+        if(root1 == null && root2 == null) return true;
+        if(root1 == null || root2 == null) return false;
+
+        if(root1.val != root2.val) return false;
+
+        TreeNode root1Left = root1.left;
+        TreeNode root1Right = root1.right;
+
+        TreeNode root2Left = root2.left;
+        TreeNode root2Right = root2.right;
+
+        int root1LeftVal = (root1Left == null)? -1 : root1Left.val;
+        int root1RightVal = (root1Right == null)? -1 : root1Right.val;
+
+        int root2LeftVal = (root2Left == null)? -1 : root2Left.val;
+        int root2RightVal = (root2Right == null)? -1 : root2Right.val;
+
+        if(root1LeftVal == root2LeftVal && root1RightVal == root2RightVal) {
+            return flipEquiv(root1Left, root2Left) && flipEquiv(root1Right, root2Right);
+        } else if(root1LeftVal == root2RightVal && root1RightVal == root2LeftVal) {
+            return flipEquiv(root1Left, root2Right) && flipEquiv(root1Right, root2Left);
+        } else {
             return false;
         }
-        if(root2 == null) {
-            return false;
-        }
-        if(root1.val != root2.val) {
-            return false;
-        }
-        
-        boolean noSwap = flipEquiv(root1.left, root2.left)
-            && flipEquiv(root1.right, root2.right);
-        
-        boolean swap = flipEquiv(root1.left, root2.right)
-            && flipEquiv(root1.right, root2.left);
-        
-        return noSwap || swap;
+
     }
 }
